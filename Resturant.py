5b684a031a2388c994349a935af755dc37c7230b
Rushikesh Karande <rushikeshkarande1061@gmail.com>
2025-04-12 22:53:34 +0530

    Update Resturant.py

--- Resturant.py
+++ Resturant.py
@@ -1,14 +1,10 @@
 from menu import menu
 
 class rest(menu):
-    def __init__(self):
-        super().__init__()  # Initialize the parent class (menu)
-
     def get(self):
-        # Calling the methods from the parent (menu) class
-        super().additem()    # Add item to the menu
-        super().showmenu()   # Show the menu
-        super().modyfyitems()  # Modify a menu item
-        super().orderitem()  # Place an order
-        super().showorder()  # Show the order summary
+        super().additem()    
+        super().showmenu()   
+        super().modyfyitems()  
+        super().orderitem()  
+        super().showorder()  
         
